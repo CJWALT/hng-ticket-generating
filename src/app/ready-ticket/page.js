dc250ee318be9534f098ffa96c445ef26721e79f
cjwalt <oyedelee22@gmail.com>
2025-02-14 13:30:12 -0800

    update

--- src/app/ready-ticket/page.js
+++ src/app/ready-ticket/page.js
@@ -39,7 +39,7 @@ const ReadyTicket = () => {
             <div className='flex jusitfy-center align-center h-auto py-[40px] sm:border-2 sm:border-red-400 md:border-blue-400 lg:border-green-400'>
            
 
-                <div className="p-[24px] rounded-[12px] border-2 self-center bg-[#08252B] mx-auto w-[90%] sm:w-[75%] overflow-hidden border-[#0E464F] text-white">
+                <div className="p-[24px] rounded-[12px] sm:h-[1400px] sm:max-w-[600] border-2 self-center bg-[#08252B] mx-auto w-[90%] sm:w-[75%] overflow-hidden border-[#0E464F] text-white">
 
                     <div className="relative border-b-2 mb-[2rem] flex flex-row justify-between  pb-3 border-[#0E464F] text-white">
                         <h2>Ready</h2>
@@ -54,15 +54,15 @@ const ReadyTicket = () => {
                     
                     </div>
 
-                    <div className="p-[20px] relative curved_inward h-[600px]">
-                        <div className="rounded-[24px] p-4 mt-[.2rem] border-2 border-[#71A7AF] ">
+                    <div className="p-[20px] relative flex justify-center sm:w-[432px] sm:bg-bottom sm:h-[670px] curved_inward h-[600px]">
+                        <div className=" sm:w-[310px] sm:h-[450px] rounded-[24px] p-4 mt-[.2rem] border-2 border-[#71A7AF] ">
                         <div className="text-center">
                             <h4 className="font-normal text-[34px] font-roadRage">Techember Fest "25</h4>
                             <p className=" font-normal text-[10px]"> <ion-icon name="pin-sharp" className='text-red-500'></ion-icon>04 Rumens road, Ikoyi, Lagos</p>
                             <small className="text-[10px] font-normal"> <ion-icon name="calendar-number-sharp" className='text-red-500'></ion-icon> &nbsp; March 15, 2021 | 7.00pm</small>
                         </div>
                         <div className="flex justify-center my-[.5rem] h-[140px] overflow-hidden">
-                        <Image src={attendeeDetails?.photo} alt='attendant' width={200} height={200} className="w-[70%] h-auto object-contain rounded-[24px] border-2 border-red"/>
+                        <Image src={attendeeDetails?.photo} alt='attendant' width={200} height={200} className="w-[70%] sm:w-[45%] h-auto object-contain rounded-[24px] border-2 border-red"/>
                         </div>
                         
                         <div className=" rounded-md border-2 border-[#133D44] bg-[#08343C] p-2">
